dc501119ad2a51e74dfd0ed847c195610cd4caad
NakaokaRei <reideeplearning@gmail.com>
2023-08-13 23:20:13 +0900

    init

---
+++ motion2operate-iOS/motion2operate-iOS/motion2operate_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  motion2operate_iOSApp.swift
+//  motion2operate-iOS
+//
+//  Created by rei.nakaoka on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct motion2operate_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ motion2operate-mac/motion2operate-mac/motion2operate_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  motion2operate_macApp.swift
+//  motion2operate-mac
+//
+//  Created by rei.nakaoka on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct motion2operate_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
